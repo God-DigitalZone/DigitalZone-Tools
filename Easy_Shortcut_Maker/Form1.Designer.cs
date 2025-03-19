0b15ec837d169fbf6abf4e0564beb6f263e89d1e
DigitalZone <digitalzonegamesnews@proton.me>
2025-03-19 14:52:26 +0100

    Added Dark Mode (Toggle) + Add Arguments (Optional)

--- Easy_Shortcut_Maker/Form1.Designer.cs
+++ Easy_Shortcut_Maker/Form1.Designer.cs
@@ -13,22 +13,28 @@
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
+
         private void InitializeComponent()
         {
             this.txtGamePath = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtArguments = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnToggleDarkMode = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtGamePath
             // 
-            this.txtGamePath.Location = new System.Drawing.Point(12, 12);
+            this.txtGamePath.Location = new System.Drawing.Point(12, 25);
             this.txtGamePath.Name = "txtGamePath";
             this.txtGamePath.Size = new System.Drawing.Size(260, 20);
             this.txtGamePath.TabIndex = 0;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Location = new System.Drawing.Point(12, 38);
+            this.btnGenerate.Location = new System.Drawing.Point(12, 116);
             this.btnGenerate.Name = "btnGenerate";
             this.btnGenerate.Size = new System.Drawing.Size(260, 23);
             this.btnGenerate.TabIndex = 1;
@@ -36,11 +42,50 @@
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
             // 
+            // txtArguments
+            // 
+            this.txtArguments.Location = new System.Drawing.Point(12, 69);
+            this.txtArguments.Name = "txtArguments";
+            this.txtArguments.Size = new System.Drawing.Size(260, 20);
+            this.txtArguments.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path of the game:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Arguments (Optional):";
+            // 
+            // btnToggleDarkMode
+            // 
+            this.btnToggleDarkMode.Location = new System.Drawing.Point(12, 145);
+            this.btnToggleDarkMode.Name = "btnToggleDarkMode";
+            this.btnToggleDarkMode.Size = new System.Drawing.Size(260, 23);
+            this.btnToggleDarkMode.TabIndex = 5;
+            this.btnToggleDarkMode.Text = "Toggle Dark Mode";
+            this.btnToggleDarkMode.UseVisualStyleBackColor = true;
+            this.btnToggleDarkMode.Click += new System.EventHandler(this.BtnToggleDarkMode_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 71);
+            this.ClientSize = new System.Drawing.Size(284, 180);
+            this.Controls.Add(this.btnToggleDarkMode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtArguments);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.txtGamePath);
             this.Name = "Form1";
@@ -48,5 +93,14 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGamePath;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.TextBox txtArguments;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnToggleDarkMode;
     }
 }
--- Easy_Shortcut_Maker/Form1.cs
+++ Easy_Shortcut_Maker/Form1.cs
@@ -3,17 +3,18 @@ using System.IO;
 using System.Windows.Forms;
 using Microsoft.CSharp;
 using System.CodeDom.Compiler;
+using System.Diagnostics;
 
 namespace Easy_Shortcut_Maker
 {
     public partial class Form1 : Form
     {
-        private TextBox txtGamePath;
-        private Button btnGenerate;
+        private bool isDarkMode = true; // Dark Mode on by default
 
         public Form1()
         {
-            InitializeComponent(); // Questo metodo è definito in Form1.Designer.cs
+            InitializeComponent();
+            ApplyDarkMode(isDarkMode); // Applies Dark Mode at Startup
         }
 
         private void BtnGenerate_Click(object sender, EventArgs e)
@@ -21,15 +22,17 @@ namespace Easy_Shortcut_Maker
             string gamePath = txtGamePath.Text;
             if (string.IsNullOrEmpty(gamePath))
             {
-                MessageBox.Show("Inserisci il percorso del gioco.");
+                MessageBox.Show("Enter the path of the game.");
                 return;
             }
 
             string shortcutExePath = Path.Combine(Application.StartupPath, "GameLauncher.exe");
             if (File.Exists(shortcutExePath))
             {
-                File.Delete(shortcutExePath); // Sovrascrive se esiste già
+                File.Delete(shortcutExePath); // Overrides if it already exists
             }
+
+            string arguments = txtArguments.Text;
 
             string code = $@"
 using System;
@@ -39,7 +42,7 @@ class Program
 {{
     static void Main()
     {{
-        Process.Start(@""{gamePath.Replace(@"\", @"\\")}"");
+        Process.Start(@""{gamePath.Replace(@"\", @"\\")}"", @""{arguments.Replace("\"", "\"\"")}"");
     }}
 }}";
 
@@ -61,14 +64,71 @@ class Program
                 CompilerResults results = provider.CompileAssemblyFromSource(parameters, code);
                 if (results.Errors.HasErrors)
                 {
-                    string errors = "Errori di compilazione:\n";
+                    string errors = "Compilation errors:\n";
                     foreach (CompilerError error in results.Errors)
                     {
-                        errors += $"- Linea {error.Line}: {error.ErrorText}\n";
+                        errors += $"- Line {error.Line}: {error.ErrorText}\n";
                     }
                     MessageBox.Show(errors);
                 }
             }
+        }
+
+        private void ToggleDarkMode()
+        {
+            isDarkMode = !isDarkMode; // Reverse the state of Dark Mode
+            ApplyDarkMode(isDarkMode); // Apply Theme
+        }
+
+        private void ApplyDarkMode(bool darkMode)
+        {
+            if (darkMode)
+            {
+                // Colors for Dark Mode
+                this.BackColor = System.Drawing.Color.FromArgb(30, 30, 30);
+                this.ForeColor = System.Drawing.Color.White;
+
+                txtGamePath.BackColor = System.Drawing.Color.FromArgb(50, 50, 50);
+                txtGamePath.ForeColor = System.Drawing.Color.White;
+
+                txtArguments.BackColor = System.Drawing.Color.FromArgb(50, 50, 50);
+                txtArguments.ForeColor = System.Drawing.Color.White;
+
+                btnGenerate.BackColor = System.Drawing.Color.FromArgb(70, 70, 70);
+                btnGenerate.ForeColor = System.Drawing.Color.White;
+
+                label1.ForeColor = System.Drawing.Color.White;
+                label2.ForeColor = System.Drawing.Color.White;
+
+                btnToggleDarkMode.BackColor = System.Drawing.Color.FromArgb(70, 70, 70);
+                btnToggleDarkMode.ForeColor = System.Drawing.Color.White;
+            }
+            else
+            {
+                // Colors for Light Mode
+                this.BackColor = System.Drawing.SystemColors.Control;
+                this.ForeColor = System.Drawing.SystemColors.ControlText;
+
+                txtGamePath.BackColor = System.Drawing.SystemColors.Window;
+                txtGamePath.ForeColor = System.Drawing.SystemColors.WindowText;
+
+                txtArguments.BackColor = System.Drawing.SystemColors.Window;
+                txtArguments.ForeColor = System.Drawing.SystemColors.WindowText;
+
+                btnGenerate.BackColor = System.Drawing.SystemColors.Control;
+                btnGenerate.ForeColor = System.Drawing.SystemColors.ControlText;
+
+                label1.ForeColor = System.Drawing.SystemColors.ControlText;
+                label2.ForeColor = System.Drawing.SystemColors.ControlText;
+
+                btnToggleDarkMode.BackColor = System.Drawing.SystemColors.Control;
+                btnToggleDarkMode.ForeColor = System.Drawing.SystemColors.ControlText;
+            }
+        }
+
+        private void BtnToggleDarkMode_Click(object sender, EventArgs e)
+        {
+            ToggleDarkMode();
         }
     }
 }
